a5eac47f2bac3362b541f66f4fdddc2704d29484
Sébastien <niou128@gmail.com>
2021-06-24 10:22:06 +0200

    Defined initial position

--- MarsRoverKata.Tests.XUnit/RoverTests.cs
+++ MarsRoverKata.Tests.XUnit/RoverTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using System.Drawing;
 using Xunit;
 
 namespace MarsRoverKata.Tests.XUnit
@@ -11,6 +12,72 @@ namespace MarsRoverKata.Tests.XUnit
             var instance = new Rover();
 
             instance.ToString().Should().NotBeNull();
+        }
+
+        [Fact(DisplayName = "Constructor should not be null")]
+        [Trait("Rover", "Constructor")]
+        public void ShouldCreateRoverWithArgumentGiven()
+        {
+            Rover rover = new Rover(new Point(0, 0), Direction.N);
+
+            rover.Should().NotBeNull();
+        }
+
+        [Fact(DisplayName = "Initial posisition should be 0,0")]
+        [Trait("Rover", "Constructor")]
+        public void InitialPositionShouldBe00()
+        {
+            Point expectedPosition = new Point(0, 0);
+
+            Rover rover = new Rover(new Point(0, 0), Direction.N);
+
+            Assert.Equal(expectedPosition, rover.Position);
+        }
+
+        [Fact(DisplayName = "Initial direction should be North")]
+        [Trait("Rover", "Constructor")]
+        public void InitialDirectionShouldBeN()
+        {
+            Direction expectedDirection = Direction.N;
+
+            Rover rover = new Rover(new Point(0, 0), Direction.N);
+
+            Assert.Equal(expectedDirection, rover.Direction);
+        }
+
+        [Fact(DisplayName = "If no argument passed to the constructor, intial position should be 0, 0")]
+        [Trait("Rover", "Constructor")]
+        public void WithoutArgumentPositionShouldBe00()
+        {
+            Point expectedPosition = new Point(0, 0);
+
+            Rover rover = new Rover();
+
+            Assert.Equal(expectedPosition, rover.Position);
+        }
+
+        [Fact(DisplayName = "Without argmuent, initial direction should be north")]
+        [Trait("Rover", "Constructor")]
+        public void WithoutArgumentDirectionShouldBeN()
+        {
+            Direction expectedDirection = Direction.N;
+
+            Rover rover = new Rover();
+
+            Assert.Equal(expectedDirection, rover.Direction);
+        }
+
+        [Theory(DisplayName = "")]
+        [Trait("Rover", "Move")]
+        [InlineData("F", "o")]
+        public void Test(string move, string a)
+        {
+            //Todo: Si direction N y+1
+            // si direction W x+1
+            // direction S y-1
+            // direction E x-1
+
+            // penser au borne 10 -> 0
         }
     }
 }
--- MarsRoverKata/Rover.cs
+++ MarsRoverKata/Rover.cs
@@ -1,6 +1,23 @@
-﻿namespace MarsRoverKata
+﻿using System.Drawing;
+
+namespace MarsRoverKata
 {
     public class Rover
     {
+        private static readonly Point DefaultInitialPosition = new Point(0, 0);
+        private const Direction DefaultInitialDirection = Direction.N;
+
+        public Point Position { get; set; }
+        public Direction Direction { get; set; }
+
+        public Rover() : this(DefaultInitialPosition, DefaultInitialDirection)
+        {
+        }
+
+        public Rover(Point point, Direction direction)
+        {
+            Position = point;
+            Direction = direction;
+        }
     }
 }
